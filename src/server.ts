e6ef9b58eb2cddf2474a055593cb2a40d0e9115c
DevTec2020 <leonardo.silva.inf@gmail.com>
2024-12-04 20:06:52 -0300

    Testando requisição GET com e sem parametros nomeados

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,35 @@
-function sum(a: number, b: number): number {
-    return a + b
-}
+import express from "express"
 
-const result: number = sum(5, 3)
 
-console.log("Resultado da soma é", result) 
+ const PORT = 3333
+
+ const app = express()
+
+/* Função da rota GET com parametros não nomeados (params)
+
+ app.get("/products/:id/:user", (request, response) => {
+
+    //recuperando os parametros fornecidos na rota
+    const {id, user} = request.params
+
+    // Retornando em tela os parametros não nomeados
+    response.send(`Produto ${id} do usuário ${user}`)
+
+ })
+
+*/
+
+ // Função da rota GET com parametros nomeados (query)
+ app.get("/products", (request, response) => {
+    // /products?page=5&limit=20
+
+    //recuperando os parametros fornecidos na rota
+    const {page, limit} = request.query
+
+    // Retornando em tela os parametros nomeados
+    response.send(`Pagina ${page} de ${limit}`)
+
+ })
+
+
+ app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
